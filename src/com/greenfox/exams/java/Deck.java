04ac62fd3db45c68d4dd7f2934154a830cbef39e
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-05 10:11:22 +0100

    Deck finished

--- src/com/greenfox/exams/java/Deck.java
+++ src/com/greenfox/exams/java/Deck.java
@@ -1,7 +1,41 @@
 package com.greenfox.exams.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by Rita on 2016-12-05.
  */
 public class Deck {
+    ArrayList<Card> cards;
+    ArrayList<Card> used;
+
+    public Deck() {
+        ArrayList<Integer> values = new ArrayList<>(Arrays.asList(2,3,4,5,6,7,8,9,10,11,12,13,14));
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("kor", "karo", "treff", "pikk"));
+        cards = new ArrayList<>();
+        for (String color : colors) {
+            for (Integer i : values) {
+                cards.add(new Card(color, i));
+            }
+        }
+        shuffle();
+        used = new ArrayList<>();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawACard() {
+        if (cards.size() > 0) {
+            Card card = cards.get(0);
+            cards.remove(0);
+            used.add(card);
+            return card;
+        } else {
+            return null;
+        }
+    }
 }
