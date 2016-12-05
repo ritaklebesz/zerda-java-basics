cec621ac20116bb519e518551f6f6b2f6653eb0e
Rita Klebesz <ritaklebesz@gmail.com>
2016-12-05 09:52:54 +0100

    Card finished

--- src/com/greenfox/exams/java/Card.java
+++ src/com/greenfox/exams/java/Card.java
@@ -1,7 +1,25 @@
 package com.greenfox.exams.java;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
 /**
  * Created by Rita on 2016-12-05.
  */
 public class Card {
+    String color;
+    int value;
+
+    public Card(String color, int value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
 }
